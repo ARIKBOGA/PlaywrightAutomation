df8f4790b3edd5c501b1c26b3f2f60232c7bb121
ARIKBOGA <barikboga42@gmail.com>
2025-01-14 16:55:21 +0300

    Implemented Actions(hover) in a sample test

--- tests/my_first_test.spec.js
+++ tests/my_first_test.spec.js
@@ -10,3 +10,14 @@ test("My First Test", async ({ page }) => {
 });
 
 
+
+test('test', async ({ page }) => {
+  await page.goto('https://practice.cydeo.com/');
+  await page.getByRole('link', { name: 'Hovers' }).click();
+  await page.getByRole('img', { name: 'User Avatar' }).first().hover();
+  await page.getByRole('link', { name: 'View profile' }).click();
+  await page.goBack();
+  await page.getByRole('img', { name: 'User Avatar' }).nth(1).hover();
+  await expect(page.locator('#content')).toContainText('name: user2');
+  await expect(page.getByRole('heading', { name: 'name: user2' })).toBeVisible();
+});
