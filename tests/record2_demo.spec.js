e573595b7ffe0c2cdf3828260d2e905e919d4b08
ARIKBOGA <barikboga42@gmail.com>
2025-01-15 19:48:11 +0300

    Added Record feature and CLI operations for Playwright test execution

---
+++ tests/record2_demo.spec.js
@@ -0,0 +1,43 @@
+
+import { test, expect } from '@playwright/test';
+import { chromium } from '@playwright/test';
+
+test('Record Demo 2',async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="username"]').press('Tab');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="item-1-img-link"]').click();
+  await page.locator('[data-test="add-to-cart"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="continue-shopping"]').click();
+  await page.getByRole('button', { name: 'Open Menu' }).click();
+  await page.locator('[data-test="logout-sidebar-link"]').click();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+});
+
+
+test.use({
+  colorScheme: 'dark'
+});
+
+test('playwright website navigation test DARK mode', async ({ page }) => {
+  await page.goto('http://playwright.dev/');
+  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Frames' }).click();
+  await page.getByRole('link', { name: 'Mock APIs' }).click();
+  await page.getByRole('link', { name: 'Page object models' }).click();
+  await page.getByRole('tab', { name: 'JavaScript' }).first().click();
+  await page.getByRole('tab', { name: 'Library' }).first().click();
+  await page.getByRole('tab', { name: 'TypeScript' }).first().click();
+});
