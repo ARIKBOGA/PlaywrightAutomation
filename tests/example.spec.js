af55cb3304382a8edb22fe64d73b86394d8d6b39
ARIKBOGA <barikboga42@gmail.com>
2025-01-16 16:15:28 +0300

    - Investigated implementation of Trace feature for Playwright - Added .zip files to .gitignore

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,31 +1,39 @@
 // @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect } = require("@playwright/test");
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page, context }) => {
+  // Start tracing
+  await context.tracing.start({ screenshots: true, snapshots: true });
+
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 
   // create a locator
-  const getStarted = page.locator('text=Get started');
+  const getStarted = page.locator("text=Get started");
 
   // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+  await expect(getStarted).toHaveAttribute("href", "/docs/intro");
 
   // Click the getStarted link
   await getStarted.click();
 
   // Expect the URL contains intro.
   await expect(page).toHaveURL(/.*intro/);
+
+  // stop tracing
+  await context.tracing.stop({path: "example-test-trace.zip"});
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
