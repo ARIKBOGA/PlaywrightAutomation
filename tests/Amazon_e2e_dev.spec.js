d018f35b9fe762ed0edbc74b66e6523410f81f8b
ARIKBOGA <barikboga42@gmail.com>
2025-01-23 20:34:50 +0300

    feat(tests): add handling for potential pop-up in Amazon e2e test

--- tests/Amazon_e2e_dev.spec.js
+++ tests/Amazon_e2e_dev.spec.js
@@ -1,13 +1,21 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
+test("Amazon", async ({ page }) => {
+  await page.goto("https://www.amazon.com/");
 
-test('Amazon', async ({ page }) => {
-    await page.goto('https://www.amazon.com/');
-    await page.getByRole('button', { name: 'Submit' }).first().click();
-    await page.locator('input[name="field-keywords"]').click();
-    await page.locator('input[name="field-keywords"]').fill('Laptop');
-    await page.locator('input[name="field-keywords"]').press('Enter');
+  // for pop-up showing up sometimes
+  // Check if the pop-up button is visible and handle it if present
+  const alertSubmitButton = page.getByRole("button", { name: "Submit" }).first();
 
-    await expect(page).toHaveTitle('Amazon.com : Laptop');
+  if (await alertSubmitButton.isVisible()) {
+    console.log("Pop-up is visible. Clicking the Submit button.");
+    await alertSubmitButton.click();
+  } else {
+    console.log("Pop-up did not appear.");
+  }
+  await page.locator('input[name="field-keywords"]').click();
+  await page.locator('input[name="field-keywords"]').fill("Laptop");
+  await page.locator('input[name="field-keywords"]').press("Enter");
 
+  await expect(page).toHaveTitle("Amazon.com : Laptop");
 });
