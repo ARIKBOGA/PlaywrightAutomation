e603d9a867b6c002232fda04be1fc0cb759e47e3
ARIKBOGA <barikboga42@gmail.com>
2025-01-26 16:36:18 +0300

    fix(config): reduce slow motion delay for improved test performance

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ module.exports = defineConfig({
     timeout: 2000, // Timeout set to 2 seconds
     video: "on",
     launchOptions: {  // for slowMo, see https://playwright.dev/docs/api/class-browsertype
-      slowMo: 1000, // Slow down the browser for better visibility
+      slowMo: 100, // Slow down the browser for better visibility
     },
   },
 
