49569eadfdcd9e0920241ad582964c0bd1836373
ARIKBOGA <barikboga42@gmail.com>
2025-01-30 00:13:45 +0300

    Added Tags for grouping tests as smoke, regression or any other feature name like login

--- tests/Annotations_Tags.spec.ts
+++ tests/Annotations_Tags.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+
+// Annotations
 test.skip("@skip this test", async () => {});
 
 test.fail("@fail this test", async () => {});
@@ -24,3 +26,18 @@ test.skip("This test will be skipped", async ({ page, browserName }) => {
 test.skip("This test will be skipped on condition", async ({ page, browserName }) => {
     test.skip(browserName === "chromium", "This test will be skipped on webkit");
 });
+
+
+// Tags
+
+test('Test login page @smoke @regression @login', async ({ page }) => {
+    console.log("This test will be tagged as smoke, regression and login");
+});
+
+// To run tests with specific tags
+// npx playwright test ./tests/Annotations_Tags.spec.ts --project chromium --grep "@login"
+// npx playwright test ./tests/Annotations_Tags.spec.ts --project chromium --grep "@smoke"
+// npx playwright test ./tests/Annotations_Tags.spec.ts --project chromium --grep "@regression"
+
+// for skipping tests thpse have given tags
+// npx playwright test ./tests/Annotations_Tags.spec.ts --project chromium --grep-invert "@login"
